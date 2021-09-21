06588f01c65e076208aa9b34850196d7872d8f51
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2021-09-21 21:57:46 +0100

    added in styling to create difference in the designs of the three

--- src/Projects.js
+++ src/Projects.js
@@ -13,13 +13,13 @@ function Projects(props) {
           link="perkk"
           image={perkk1}
           title="Perkk"
-          description="An app giving greater choice of workplace perks to employees"
+          description="Perkk was a project that I pitched with an aim to give employees greater choice in their workplace perks. After being chosen it was worked on by a team of four. My role was that of team lead which required clear management of planning and the github workflow. The project was compelted a day early with the major challenges being the integration of a range of JS functionality to bring the project to life."
         />
         <Thumbnail
           link="movies"
           image={movies1}
           title="Movies"
-          description="A quick personal project to make a movie watch list"
+          description="A quick personal app to make a movie watch list. Pulling data from The Movie Database API it allowed for movies to be sorted into categories and saved for later. An early project that I undertook it cemented a lot of knowledge about CSS and gave a good bedrock for further development."
         />
       </div>
     </div>
