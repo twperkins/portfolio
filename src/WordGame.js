1c07544ea544776f3365af237e8e10a9c824e9cd
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2023-09-13 18:37:33 +0100

    update styling of word game to increase interactivity

--- src/WordGame.js
+++ src/WordGame.js
@@ -7,6 +7,7 @@ const WordGame = () => {
   const [definition, setDefinition] = useState(null);
   const [currentInput, setCurrentInput] = useState(Array(words.length).fill(""));
   const [wordGuessed, setWordGuessed] = useState(false);
+  const [displayGame, setDisplayGame] = useState(false);
 
   useEffect(() => {
     fetch('https://random-word-api.herokuapp.com/word?number=20&length=9')
@@ -88,26 +89,39 @@ const WordGame = () => {
     }
   };
 
+  const showGame = () => {
+    setDisplayGame(!displayGame)
+  }
+
 
   return (
     <>
       <div className="word-container">
-        <div className="letter-container">
-          {word?.split("").map((letter, index) => (
-            <div key={`container${index}`}>
-              <input
-                className={`letter-input ${wordGuessed ? 'guessed' : ''}`}
-                key={`input${index}`}
-                maxLength={1}
-                value={currentInput[index]}
-                onKeyDown={(e) => handleNextInput(index, e)}
-                onKeyUp={(e) => handleWordCheck()}
-                ref={(inputRef) => (inputRefs.current[index] = inputRef)}
-              />
-            </div>
-          ))}
+        <div className={displayGame ? 'game-toggle' : 'game-toggle opposite'} onClick={() => showGame()}>&#x2963;</div>
+        <div className={displayGame ? 'game-title-container hidden' : 'game-title-container'}>
+          <div className={displayGame ? 'game-title hidden' : 'game-title'}>can you guess the word?</div>
         </div>
-        <div className="definition">definition: {definition?.toLowerCase()}</div>
+        <div className="game-container">
+          <div className={displayGame ? 'letter-container' : 'letter-container hidden'}>
+            {word?.split("").map((letter, index) => (
+              <div key={`container${index}`}>
+                <input
+                  className={`letter-input ${wordGuessed ? 'guessed' : ''}`}
+                  key={`input${index}`}
+                  maxLength={1}
+                  value={currentInput[index]}
+                  onKeyDown={(e) => handleNextInput(index, e)}
+                  onKeyUp={(e) => handleWordCheck()}
+                  ref={(inputRef) => (inputRefs.current[index] = inputRef)}
+                />
+              </div>
+            ))}
+          </div>
+          <div className={displayGame ? 'definition-container' : 'definition-container hidden'}>
+            <div className="definition-title">definition</div>
+            <div className="definition-content">{definition?.toLowerCase()}</div>
+          </div>
+        </div>
       </div>
     </>
   );
