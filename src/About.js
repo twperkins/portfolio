ffb6fbe4690e0f7512157937c95389cd3fac6b90
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2023-09-16 13:38:52 +0100

    Make responsive

--- src/About.js
+++ src/About.js
@@ -5,12 +5,12 @@ const About = ({ displaySection }) => {
   return (
     <div className={displaySection ? 'about-container' : 'about-container hidden'}>
       <div className="first-section">
-        <div className="section-header">Welcome</div>
+        <div className="section-header">welcome</div>
         <div>Hi, Tom here. After studying history at Oxford and working for 4 years in client facing roles, obviously the next logical step was to learn to code...</div>
         <div>Curently working in a small team of engineers building the future of retail</div>
       </div>
       <div className="second-section">
-        <div className="section-header">Skills</div>
+        <div className="section-header">skills</div>
         <div className="skills">
           <div>Ruby | JavaScript | Typescript | Python</div>
           <div>Rails | React | Angular</div>
