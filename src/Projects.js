80718d2f5e557a87ed2af53198f9fc2cc38ee74b
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2023-10-05 22:20:46 +0100

    styling completed for projects

--- src/Projects.js
+++ src/Projects.js
@@ -8,51 +8,53 @@ import movies2 from "./assets/portfolio/movies2.png"
 const ProjectDetails = [
   {
     id: 0,
-    image: perkk1,
-    header: 'perkk',
+    image: perkk2,
+    header: 'appear here',
     copy: 'blah blah blah blah blah blah blah',
     link: '',
-    linkName: 'github',
+    linkName: 'website',
   },
   {
     id: 1,
-    image: movies1,
-    header: 'movies',
-    copy: 'blah blah blah blah blah blah blah',
+    image: perkk1,
+    header: 'perkk',
+    copy: 'Perkk was a project that I pitched with an aim to give employees greater choice in their workplace perks. After being chosen it was worked on by a team of four. My role was that of team lead which required clear management of planning and the github workflow. The project was compelted a day early with the major challenges being the integration of a range of JS functionality to bring the project to life.',
     link: '',
     linkName: 'github',
   },
   {
     id: 2,
-    image: perkk2,
+    image: movies2,
     header: 'finbourne',
-    copy: 'blah blah blah blah blah blah blah',
+    copy: 'blah blah blah',
     link: '',
     linkName: 'website',
   },
   {
     id: 3,
-    image: movies2,
-    header: 'appear here',
-    copy: 'blah blah blah blah blah blah blah',
+    image: movies1,
+    header: 'movies',
+    copy: 'A quick personal app to make a movie watch list. Pulling data from The Movie Database API it allowed for movies to be sorted into categories and saved for later. An early project that I undertook it cemented a lot of knowledge about CSS and gave a good bedrock for further development.',
     link: '',
-    linkName: 'website',
+    linkName: 'github',
   }
 ]
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null)
 
-  const openProject = (project) => project === selectedProject ? setSelectedProject(null) : setSelectedProject(project);
+  const openProject = (project) => setSelectedProject(project);
 
 
   return (
     <div className="projects-container">
-      <div className="projects-header">projects</div>
+      <div className="projects-header">
+        <div>{selectedProject?.header || 'projects'}</div>
+      </div>
       <div className="projects-section" >
         {
           ProjectDetails.map((project) => (
-            <div className="project-container" key={`container-${project.id}`}>
+            <div className={selectedProject === project ? "project-container selected" : selectedProject ? "project-container hidden" : "project-container"} key={`container-${project.id}`}>
               <img
                 className="project-image"
                 src={project.image} alt={project.alt}
@@ -62,6 +64,21 @@ const Projects = () => {
               <div className="project-header">{project.header}</div>
             </div>
           ))
+        }
+      </div>
+      <div className={selectedProject ? "selected-project-container" : "selected-project-container hidden"}>
+        {selectedProject &&
+          <div className="selected-project-top">
+            <img
+              className="selected-project-image"
+              src={selectedProject.image} alt={selectedProject.alt}
+            />
+            <div>
+              <div className="selected-project-return" onClick={() => openProject(null)}>&#x2962; return</div>
+              <div className="selected-project-copy">{selectedProject.copy}</div>
+              <a className="selected-project-link" href={selectedProject.link}>{selectedProject.linkName}</a>
+            </div>
+          </div>
         }
       </div>
     </div>
